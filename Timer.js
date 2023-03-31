7a1883882c1be6e8786dc73eee598de2ba29a792
DipLogan <ghoshdipu95@gmail.com>
2023-04-01 00:58:41 +0530

    Landing Page updated

--- Timer.js
+++ Timer.js
@@ -1,23 +1,24 @@
 var countDownDate = new Date("nov 12, 2023 18:30:30").getTime();
+var expiredArea = document.getElementById("demo");
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-var x = setInterval(function(){
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var now = new Date().getTime();
-    var distance = countDownDate -  now;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / (1000));
-
-    document.getElementById("days").innerHTML=days;
-    document.getElementById("hours").innerHTML=hours;
-    document.getElementById("minutes").innerHTML=minutes;
-    document.getElementById("seconds").innerHTML=seconds;
-
-    if(distance < 0){
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-
+  if (distance < 0) {
+    clearInterval(x);
+    expiredArea.innerHTML = "EXPIRED";
+    expiredArea.style.fontSize = "40px";
+    document.getElementById("timer").style.display = "none";
+  }
 }, 1000);
